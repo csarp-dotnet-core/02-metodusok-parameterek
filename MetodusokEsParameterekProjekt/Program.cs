4fbf4a18bab6a529713d8e3f6fcae8c020257c32
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-09-10 11:09:54 +0200

    Paraméter nélküli metódus

--- MetodusokEsParameterekProjekt/Program.cs
+++ MetodusokEsParameterekProjekt/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Paraméter nélküli metódus
+static void Udvozlet()
+{
+    Console.WriteLine("Köszöntelek a programban!");
+}
+
+Udvozlet();
